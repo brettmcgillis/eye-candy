dfa499c4ca6779bb522b3e0e7ed142aae990ef90
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-08 17:43:47 -0500

    add canvas & sphere?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Canvas } from '@react-three/fiber'
 
 function App() {
   return (
     <div className="App">
-      
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <sphereGeometry />
+          <meshPhysicalMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }

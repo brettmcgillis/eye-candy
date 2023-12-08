6ef63ae908d4d5ca2a260b3c3e55a88c477d4df9
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-08 17:58:30 -0500

    use cube, add camera controls

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber'
+import { CameraControls } from '@react-three/drei';
 
 function App() {
   return (
     <div className="App">
-      <Canvas>
+      <Canvas shadows>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <sphereGeometry />
+        <mesh castShadow receiveShadow>
+          <boxGeometry args={[2, 2, 2]} />
           <meshPhysicalMaterial />
         </mesh>
+``      <CameraControls makeDefault />
       </Canvas>
     </div>
   );

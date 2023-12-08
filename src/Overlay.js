f4b52876d3f92f19f9677dda9033011d0f6f0084
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-08 17:10:24 -0500

    classic

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,10 +1,12 @@
+import './Overlay.css';
+
 function Overlay() {
     const date = new Date();
     return (
-      <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-        <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>🤘 — 🔥 — 💀</div>
-        <div style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>Brett McGillis</div>
-        <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>{`${date.getDate()} | ${date.getMonth() + 1} | ${date.getFullYear()}`}</div>
+      <div className='Overlay'>
+        <div className='Top-Left'>🤘 — 🔥 — 💀</div>
+        <div className='Bottom-Left'>Brett McGillis</div>
+        <div className='Bottom-Right'>{`${date.getDate()} | ${date.getMonth() + 1} | ${date.getFullYear()}`}</div>
       </div>
     )
   }

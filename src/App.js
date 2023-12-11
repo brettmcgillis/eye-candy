c98135253ba6797b6a4b429f801557bcb1088867
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 17:04:28 -0500

    show scene

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import { useControls } from "leva";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
 
 import "./App.css";
 
-import { _45_deg } from "./utils/constants";
-
 import Loader from "./components/Loader/Loader";
 import LightingRig from "./components/Rigging/LightingRig";
 import GridHelper from "./components/Rigging/GridHelper";
 
-import { HaloSet } from "./components/Elements/Halo";
-import { Skull } from "./components/Elements/Skull";
-import { Femur } from "./components/Elements/Femur";
+import NewScene from "./scenes/NewScene";
+import { useControls } from "leva";
+import PolarGridHelper from "./components/Rigging/PolarGridHelper";
 
 function App() {
-  const femur = useControls("Femur", {
-    x: { value: 0, min: -360, max: 360, step: 1 },
-    y: { value: 0, min: -360, max: 360, step: 1 },
-    z: { value: -20, min: -360, max: 360, step: 1 },
-  });
+  // COMPLEX CONTROLS~!
+  // const controls = useControls("Controls", {
+  //   show: { value: true, label: "Show color" },
+  //   color: { value: "#fff", render: (get) => get("Controls.show") },
+  //   show2: { value: false, label: "Show folder" },
+  //   folder: folder(
+  //     {
+  //       number: 1,
+  //       string: {
+  //         value: "shown if `number >= 1`",
+  //         render: (get) => get("Controls.folder.number") >= 1,
+  //       },
+  //     },
+  //     { render: (get) => get("Controls.show2") }
+  //   ),
+  // });
+
+  const controls = useControls(
+    "Environment",
+    {
+      show_grid_helper: true,
+      show_polar_grid_helper: false,
+    },
+    { collapsed: true }
+  );
 
   return (
     <div className="App">
       <Canvas shadows>
         <Suspense fallback={<Loader />}>
-          <GridHelper />
+          <GridHelper visible={controls.show_grid_helper} />
+          <PolarGridHelper visible={controls.show_polar_grid_helper} />
           <LightingRig />
 
-          <HaloSet position={[0, 1.5, -1]} rotation={[_45_deg, 0, 0]} />
-          <Skull position={[0, 0, 0]} scale={0.1} />
-          <Femur
-            position={[-3, -3, -0.5]}
-            scale={0.75}
-            rotation={[femur.x, femur.y, femur.z]}
-          />
+          <NewScene />
 
           <CameraControls makeDefault />
         </Suspense>

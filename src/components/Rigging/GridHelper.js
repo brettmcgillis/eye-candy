12eb0f0f599fc30183bc4ba1a7c8ad26845cbe02
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 13:11:00 -0500

    huge props

--- src/components/Rigging/GridHelper.js
+++ src/components/Rigging/GridHelper.js
@@ -1,11 +1,11 @@
 import { _90_deg } from "../../utils/constants";
 
-function GridHelper() {
+function GridHelper({ size = 10, divisions = 10 }) {
   return (
     <group>
-      <gridHelper rotation={[0, 0, 0]} />
-      <gridHelper rotation={[0, 0, _90_deg]} />
-      <gridHelper rotation={[_90_deg, 0, 0]} />
+      <gridHelper args={[size, divisions]} rotation={[0, 0, 0]} />
+      <gridHelper args={[size, divisions]} rotation={[0, 0, _90_deg]} />
+      <gridHelper args={[size, divisions]} rotation={[_90_deg, 0, 0]} />
     </group>
   );
 }

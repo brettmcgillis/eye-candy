a71f2d1add32af66a30df298f22fe4865d9d579f
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 13:11:15 -0500

    clicker

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,14 +1,38 @@
-import './Overlay.css';
+import { Leva } from "leva";
+
+import "./Overlay.css";
+import { useState } from "react";
 
 function Overlay() {
-    const date = new Date();
-    return (
-      <div className='Overlay'>
-        <div className='Top-Left'>🤘 — 🔥 — 💀</div>
-        <div className='Bottom-Left'>Brett McGillis</div>
-        <div className='Bottom-Right'>{`${date.getDate()} | ${date.getMonth() + 1} | ${date.getFullYear()}`}</div>
+  const date = new Date();
+
+  const [showLeva, setShowLeva] = useState(true);
+  const handleDebug = () => {
+    setShowLeva(!showLeva);
+  };
+
+  return (
+    <>
+      {/* 
+        TODO: add collapsed attribute
+        set showLeva default false
+       */}
+      <Leva hidden={!showLeva} />
+      <div className="Overlay">
+        <div className="Top-Left">
+          🤘 —
+          <span className="Debug" onClick={handleDebug}>
+            &nbsp;🔥&nbsp;
+          </span>
+          — 💀
+        </div>
+        <div className="Bottom-Left">Brett McGillis</div>
+        <div className="Bottom-Right">
+          {`${date.getDate()} | ${date.getMonth() + 1} | ${date.getFullYear()}`}
+        </div>
       </div>
-    )
-  }
-  
-  export default Overlay;
+    </>
+  );
+}
+
+export default Overlay;

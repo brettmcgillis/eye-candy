4ee96a617a75fc38f95455149646f4ea38060268
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 17:03:22 -0500

    update grids

--- src/components/Rigging/GridHelper.js
+++ src/components/Rigging/GridHelper.js
@@ -1,8 +1,8 @@
 import { _90_deg } from "../../utils/constants";
 
-function GridHelper({ size = 10, divisions = 10 }) {
+function GridHelper({ size = 10, divisions = 10, ...props }) {
   return (
-    <group>
+    <group {...props}>
       <gridHelper args={[size, divisions]} rotation={[0, 0, 0]} />
       <gridHelper args={[size, divisions]} rotation={[0, 0, _90_deg]} />
       <gridHelper args={[size, divisions]} rotation={[_90_deg, 0, 0]} />
--- src/components/Rigging/PolarGridHelper.js
+++ src/components/Rigging/PolarGridHelper.js
@@ -1,8 +1,8 @@
 import { _90_deg } from "../../utils/constants";
 
-function PolarGridHelper({ size = 10, divisions = 10 }) {
+function PolarGridHelper({ size = 10, divisions = 10, ...props }) {
   return (
-    <group>
+    <group {...props}>
       <polarGridHelper args={[size, divisions]} rotation={[0, 0, 0]} />
       <polarGridHelper args={[size, divisions]} rotation={[0, 0, _90_deg]} />
       <polarGridHelper args={[size, divisions]} rotation={[_90_deg, 0, 0]} />

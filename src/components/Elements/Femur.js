3fdb5fc2cba90715444ba652066670ce0d5a7efc
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 16:23:01 -0500

    fix model

--- src/components/Elements/Femur.js
+++ src/components/Elements/Femur.js
@@ -1,20 +1,57 @@
 import React from "react";
 import { useGLTF } from "@react-three/drei";
-
 import femur from "../../models/Femur.gltf";
 
 export function Femur(props) {
   const { nodes, materials } = useGLTF(femur);
   return (
     <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.mesh_0.geometry}
-        material={materials.material_0}
-      />
+      <group position={[-4.28, 3.792, 4.512]} rotation={[-3.127, 0.467, 1.865]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_2.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_3.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_4.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_5.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_6.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_7.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_8.geometry}
+          material={materials.material_0}
+        />
+      </group>
     </group>
   );
 }
 
-useGLTF.preload("/models/Femur.gltf");
+useGLTF.preload("/Femur.gltf");

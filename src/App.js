2db4557f0cd0a900fd3e8aa85e8b5dacf4c54020
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 10:51:56 -0500

    add loader and rejig

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useControls } from "leva";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
 
 import "./App.css";
-import { _45_deg, _90_deg } from "./utils/constants";
+import { _45_deg } from "./utils/constants";
 
-import { GridHelper } from "./components/GridHelper";
-import { HaloSet } from "./components/Halo";
-import { Skull } from "./components/Skull";
-import { Femur } from "./components/Femur";
-import LightingRig from "./components/LightingRig";
-import { useControls } from "leva";
+import LightingRig from "./components/Rigging/LightingRig";
+import GridHelper from "./components/Rigging/GridHelper";
+import { HaloSet } from "./components/Elements/Halo";
+import { Skull } from "./components/Elements/Skull";
+import { Femur } from "./components/Elements/Femur";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   const femur = useControls("Femur", {
@@ -17,24 +19,24 @@ function App() {
     y: { value: 0, min: -360, max: 360, step: 1 },
     z: { value: -20, min: -360, max: 360, step: 1 },
   });
-  // const skull = useControls("Skull", {});
-  // const halo = useControls("Halo", {});
 
   return (
     <div className="App">
       <Canvas shadows>
-        <GridHelper />
-        <LightingRig />
+        <Suspense fallback={<Loader />}>
+          <GridHelper />
+          <LightingRig />
 
-        <HaloSet position={[0, 1.5, -1]} rotation={[_45_deg, 0, 0]} />
-        <Skull position={[0, 0, 0]} scale={0.1} />
-        <Femur
-          position={[-3, -3, -0.5]}
-          scale={0.75}
-          rotation={[femur.x, femur.y, femur.z]}
-        />
+          <HaloSet position={[0, 1.5, -1]} rotation={[_45_deg, 0, 0]} />
+          <Skull position={[0, 0, 0]} scale={0.1} />
+          <Femur
+            position={[-3, -3, -0.5]}
+            scale={0.75}
+            rotation={[femur.x, femur.y, femur.z]}
+          />
 
-        <CameraControls makeDefault />
+          <CameraControls makeDefault />
+        </Suspense>
       </Canvas>
     </div>
   );

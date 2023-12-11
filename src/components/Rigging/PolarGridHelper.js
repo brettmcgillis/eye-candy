c245cadc6447081e540ab73d82133d5504c4eeb4
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 15:59:35 -0500

    add polar grid helper

---
+++ src/components/Rigging/PolarGridHelper.js
@@ -0,0 +1,13 @@
+import { _90_deg } from "../../utils/constants";
+
+function PolarGridHelper({ size = 10, divisions = 10 }) {
+  return (
+    <group>
+      <polarGridHelper args={[size, divisions]} rotation={[0, 0, 0]} />
+      <polarGridHelper args={[size, divisions]} rotation={[0, 0, _90_deg]} />
+      <polarGridHelper args={[size, divisions]} rotation={[_90_deg, 0, 0]} />
+    </group>
+  );
+}
+
+export default PolarGridHelper;

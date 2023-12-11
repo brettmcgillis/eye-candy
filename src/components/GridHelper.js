c132b676157782bd8043fc6d21ccfcc76ef3fe13
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 09:38:20 -0500

    add grid helper

---
+++ src/components/GridHelper.js
@@ -0,0 +1,11 @@
+import { _90_deg } from "../utils/constants";
+
+export function GridHelper() {
+  return (
+    <group>
+      <gridHelper rotation={[0, 0, 0]} />
+      <gridHelper rotation={[0, 0, _90_deg]} />
+      <gridHelper rotation={[_90_deg, 0, 0]} />
+    </group>
+  );
+}

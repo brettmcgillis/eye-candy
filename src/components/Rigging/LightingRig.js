09d834118c9ed6e52bd7c5e5e9f06f1e40bdb202
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 17:03:48 -0500

    update lighting

--- src/components/Rigging/LightingRig.js
+++ src/components/Rigging/LightingRig.js
@@ -1,18 +1,28 @@
 import { useControls } from "leva";
 
 function LightingRig() {
-  const pointLight = useControls("Point Light", {
-    x: { value: 3, min: -10, max: 10, step: 1 },
-    y: { value: 3, min: -10, max: 10, step: 1 },
-    z: { value: 5, min: -10, max: 10, step: 1 },
-    decay: { value: 0, min: -10, max: 10, step: 1 },
-    distance: { value: -1, min: -10, max: 10, step: 1 },
-    castShadow: true,
-  });
+  const pointLight = useControls(
+    "Point Light",
+    {
+      x: { value: 3, min: -10, max: 10, step: 1 },
+      y: { value: 3, min: -10, max: 10, step: 1 },
+      z: { value: 5, min: -10, max: 10, step: 1 },
+      decay: { value: 0, min: -10, max: 10, step: 1 },
+      distance: { value: -1, min: -10, max: 10, step: 1 },
+      castShadow: true,
+    },
+    {
+      collapsed: true,
+    }
+  );
 
-  const ambientLight = useControls("Ambient Light", {
-    intensity: { value: 0.1, min: 0, max: 1, step: 0.1 },
-  });
+  const ambientLight = useControls(
+    "Ambient Light",
+    {
+      intensity: { value: 0.1, min: 0, max: 1, step: 0.1 },
+    },
+    { collapsed: true }
+  );
 
   return (
     <group>

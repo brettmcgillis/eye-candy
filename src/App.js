c861535230cba8aad737a7119a1e62b11f29aaef
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 09:38:47 -0500

    use lighting rig, constants

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import { Canvas } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
+
 import "./App.css";
+import { _45_deg, _90_deg } from "./utils/constants";
+
+import { GridHelper } from "./components/GridHelper";
 import { HaloSet } from "./components/Halo";
 import { Skull } from "./components/Skull";
 import { Femur } from "./components/Femur";
-
-const _45_deg = Math.PI / 4;
-const _90_deg = Math.PI / 2;
+import LightingRig from "./components/LightingRig";
+import { useControls } from "leva";
 
 function App() {
+  const femur = useControls("Femur", {
+    x: { value: 0, min: -360, max: 360, step: 1 },
+    y: { value: 0, min: -360, max: 360, step: 1 },
+    z: { value: -20, min: -360, max: 360, step: 1 },
+  });
+  // const skull = useControls("Skull", {});
+  // const halo = useControls("Halo", {});
+
   return (
     <div className="App">
       <Canvas shadows>
-        <gridHelper rotation={[0, 0, 0]} />
-        <gridHelper rotation={[0, 0, _90_deg]} />
-        <gridHelper rotation={[_90_deg, 0, 0]} />
-
-        <ambientLight intensity={1} />
+        <GridHelper />
+        <LightingRig />
 
         <HaloSet position={[0, 1.5, -1]} rotation={[_45_deg, 0, 0]} />
         <Skull position={[0, 0, 0]} scale={0.1} />
-        <Femur position={[-3, -3, -0.5]} scale={0.75} rotation={[0, 0, 0]} />
+        <Femur
+          position={[-3, -3, -0.5]}
+          scale={0.75}
+          rotation={[femur.x, femur.y, femur.z]}
+        />
 
         <CameraControls makeDefault />
       </Canvas>

0002d7a6d786fb818f08f8e4418d6c7383f466c9
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-11 10:51:06 -0500

    add loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,21 @@
+import { Html, useProgress } from "@react-three/drei";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Loader.css";
+
+function Loader() {
+  const { progress } = useProgress();
+  const _progress = Math.floor(progress);
+  return (
+    <Html center>
+      <div className="Loader">
+        <div className="Text-container">Loading</div>
+        <ProgressBar now={_progress} />
+        <div className="Text-container">{_progress} %</div>
+      </div>
+    </Html>
+  );
+}
+
+export default Loader;

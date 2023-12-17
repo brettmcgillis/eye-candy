38a3f36a7c00016f3b3621da96d10faa0d669475
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-17 11:11:22 -0500

    bg color

--- src/components/Rigging/SceneEnvironment.js
+++ src/components/Rigging/SceneEnvironment.js
@@ -5,6 +5,7 @@ function SceneEnvironment() {
   const controls = useControls("Environment", {}, { collapsed: true });
   return (
     <>
+      <color attach={"background"} args={["#000000"]} />
       <Environment background blur={1} preset="city" />
     </>
   );

27491910a8136c5d07f4bb03cd27149d9a8b1f4c
Brett McGillis <brett.mcgillis@gmail.com>
2024-01-17 18:00:08 -0500

    add paper frame model

---
+++ src/components/Elements/PaperFrame.js
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+import paperFrame from "../../models/FoldedFrame.glb";
+
+export function PaperFrame(props) {
+  const { nodes, materials } = useGLTF(paperFrame);
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Frame.geometry}
+        material={materials["Material.002"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/foldedframe.glb");

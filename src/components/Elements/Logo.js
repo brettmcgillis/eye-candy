f13f6d875710c45c26f7b3d83d6d5f6167086097
Brett McGillis <brett.mcgillis@gmail.com>
2024-01-22 19:51:55 -0500

    huge props

--- src/components/Elements/Logo.js
+++ src/components/Elements/Logo.js
@@ -1,30 +1,43 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
 import logo from "../../models/Logo.glb";
 
-export default function Logo({ ...props }) {
-  const glowRed = new THREE.MeshBasicMaterial({
-    color: new THREE.Color(5, 0, 0),
-    toneMapped: false,
-    side: THREE.DoubleSide,
-  });
-
-  const black = new THREE.MeshBasicMaterial({
-    color: new THREE.Color(0, 0, 0),
-    side: THREE.DoubleSide,
-  });
-
+export default function Logo({
+  innerColorVal,
+  innerColorEmissive,
+  innerColorEmissiveIntensity,
+  outerColorVal,
+  outerColorEmissive,
+  outerColorEmissiveIntensity,
+  ...props
+}) {
   const { nodes, materials } = useGLTF(logo);
+  const innerMaterial = !innerColorVal
+    ? materials["SVGMat.001"]
+    : new THREE.MeshStandardMaterial({
+        color: innerColorEmissive ? null : innerColorVal,
+        emissive: innerColorEmissive ? innerColorVal : null,
+        emissiveIntensity: innerColorEmissiveIntensity,
+        side: THREE.DoubleSide,
+      });
+  const outerMaterial = !outerColorVal
+    ? materials["SVGMat.004"]
+    : new THREE.MeshStandardMaterial({
+        color: outerColorEmissive ? null : outerColorVal,
+        emissive: outerColorEmissive ? outerColorVal : null,
+        emissiveIntensity: outerColorEmissiveIntensity,
+        side: THREE.DoubleSide,
+      });
+
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes["frog-in"].geometry}
-        // material={materials["SVGMat.001"]}
-        material={glowRed}
+        material={innerMaterial}
         position={[0.835, 0.03, 0.46]}
         scale={[10, 1.018, 10]}
       />
@@ -32,8 +45,7 @@ export default function Logo({ ...props }) {
         castShadow
         receiveShadow
         geometry={nodes["frog-out"].geometry}
-        // material={materials["SVGMat.004"]}
-        material={black}
+        material={outerMaterial}
         position={[0.835, 0, 0.46]}
         scale={[10, 1.018, 10]}
       />
@@ -41,8 +53,7 @@ export default function Logo({ ...props }) {
         castShadow
         receiveShadow
         geometry={nodes["bret-in"].geometry}
-        // material={materials["SVGMat.002"]}
-        material={glowRed}
+        material={innerMaterial}
         position={[0.038, 0.03, 0.028]}
         scale={[10, 0.524, 10]}
       />
@@ -50,8 +61,7 @@ export default function Logo({ ...props }) {
         castShadow
         receiveShadow
         geometry={nodes["bret-out"].geometry}
-        // material={materials["SVGMat.003"]}
-        material={black}
+        material={outerMaterial}
         position={[0.045, 0, 0.099]}
         scale={[10, 0.524, 10]}
       />
--- src/components/Scenes/Loglow.js
+++ src/components/Scenes/Loglow.js
@@ -1,12 +1,13 @@
 import { extend } from "@react-three/fiber";
 import { BakeShadows, Effects } from "@react-three/drei";
 import { UnrealBloomPass } from "three-stdlib";
+import { useControls } from "leva";
 
 import { _90_deg } from "../../utils/math";
+
 import Logo from "../elements/Logo";
 import CameraRig from "../rigging/CameraRig";
 import LightingRig from "../rigging/LightingRig";
-import { useControls } from "leva";
 
 extend({ UnrealBloomPass });
 
@@ -19,6 +20,12 @@ export default function LoGlow() {
     bloomThreshold,
     bloomStrength,
     bloomRadius,
+    innerColorVal,
+    innerColorEmissive,
+    innerColorEmissiveIntensity,
+    outerColorVal,
+    outerColorEmissive,
+    outerColorEmissiveIntensity,
   } = useControls(
     "LoGlow",
     {
@@ -29,9 +36,38 @@ export default function LoGlow() {
       bloomThreshold: { label: "Bloom Threshold", value: 0.1, min: 0, max: 10 },
       bloomStrength: { label: "Bloom Strength", value: 0.2, min: 0, max: 10 },
       bloomRadius: { label: "Bloom Radius", value: 0.2, min: 0, max: 10 },
+      innerColorVal: {
+        label: "Inner Color",
+        value: "#FF0000",
+      },
+      innerColorEmissive: {
+        label: "Inner Color Emissive",
+        value: true,
+      },
+      innerColorEmissiveIntensity: {
+        label: "Inner Color Emissive Intensity",
+        value: 5,
+        min: 0,
+        max: 10,
+      },
+      outerColorVal: {
+        label: "Outer Color",
+        value: "#000000",
+      },
+      outerColorEmissive: {
+        label: "Outer Color Emissive",
+        value: false,
+      },
+      outerColorEmissiveIntensity: {
+        label: "Outer Color Emissive Intensity",
+        value: 0,
+        min: 0,
+        max: 10,
+      },
     },
     { collapsed: true }
   );
+
   return (
     <>
       <LightingRig />
@@ -40,7 +76,16 @@ export default function LoGlow() {
       <color attach="background" args={[backgroundColor]} />
       <fog attach="fog" args={[fogColor, fogNear, fogFar]} />
 
-      <Logo scale={2} rotation={[_90_deg, 0, 0]} />
+      <Logo
+        innerColorVal={innerColorVal}
+        innerColorEmissive={innerColorEmissive}
+        innerColorEmissiveIntensity={innerColorEmissiveIntensity}
+        outerColorVal={outerColorVal}
+        outerColorEmissive={outerColorEmissive}
+        outerColorEmissiveIntensity={outerColorEmissiveIntensity}
+        scale={2}
+        rotation={[_90_deg, 0, 0]}
+      />
       <BakeShadows />
       <Effects disableGamma>
         <unrealBloomPass

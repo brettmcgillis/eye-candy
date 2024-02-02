1d849b5ccf67a7482d23c4674c9cf7cfaf458adf
Brett McGillis <brett.mcgillis@gmail.com>
2024-02-01 19:03:23 -0500

    cleanup

--- src/components/Elements/Record.js
+++ src/components/Elements/Record.js
@@ -8,17 +8,12 @@ export default function Record({ ...props }) {
   const { nodes, materials } = useGLTF(record);
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-0.824, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes['REC33#RECTextures'].geometry}
-            material={materials.RECTextures}
-            rotation={[-Math.PI / 2, 0, 0]}
-          />
-        </group>
-      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes['REC33#RECTextures'].geometry}
+        material={materials.RECTextures}
+      />
     </group>
   );
 }

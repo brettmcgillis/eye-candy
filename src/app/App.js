9573159d2b919ddf8e5038d0875b789fffadeb93
Brett McGillis <brett.mcgillis@gmail.com>
2026-01-09 15:01:53 -0500

    add shitty scene selection

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,34 @@
+import { useControls } from 'leva';
 import React, { Suspense } from 'react';
 
 import { Canvas } from '@react-three/fiber';
 
 import Loader from 'app/scaffold/loader/Loader';
 
-// import DumpsterFire from 'components/scenes/DumpsterFire/DumpsterFire';
-// import FoldedFrame from 'components/scenes/FoldedFrame/FoldedFrame';
-// import LoGlow from 'components/scenes/Loglow';
-// import NewScene from 'components/scenes/NewScene';
+import DumpsterFire from 'components/scenes/DumpsterFire/DumpsterFire';
+import FoldedFrame from 'components/scenes/FoldedFrame/FoldedFrame';
+import LoGlow from 'components/scenes/Loglow';
+import NewScene from 'components/scenes/NewScene';
+import PaperStack from 'components/scenes/PaperStack/PaperStack';
 import PixelHater from 'components/scenes/PixelHater/PixelHater';
 
-// import PaperStack from 'components/scenes/paperstack/PaperStack';
 import './App.css';
 
 function App() {
+  const { scene } = useControls('Scene Selection', {
+    scene: {
+      options: {
+        PixelHater: 'PixelHater',
+        DumpsterFire: 'DumpsterFire',
+        FoldedFrame: 'FoldedFrame',
+        LoGlow: 'LoGlow',
+        NewScene: 'NewScene',
+        PaperStack: 'PaperStack',
+      },
+      value: null,
+    },
+  });
+
   return (
     <div className="App">
       <Canvas
@@ -21,12 +36,12 @@ function App() {
         gl={{ preserveDrawingBuffer: true, depth: true, debug: true }}
       >
         <Suspense fallback={<Loader />}>
-          {/* <FoldedFrame /> */}
-          {/* <LoGlow /> */}
-          {/* <NewScene /> */}
-          {/* <PaperStack /> */}
-          {/* <DumpsterFire /> */}
-          <PixelHater />
+          {scene === 'PixelHater' && <PixelHater />}
+          {scene === 'DumpsterFire' && <DumpsterFire />}
+          {scene === 'FoldedFrame' && <FoldedFrame />}
+          {scene === 'LoGlow' && <LoGlow />}
+          {scene === 'NewScene' && <NewScene />}
+          {scene === 'PaperStack' && <PaperStack />}
         </Suspense>
       </Canvas>
     </div>

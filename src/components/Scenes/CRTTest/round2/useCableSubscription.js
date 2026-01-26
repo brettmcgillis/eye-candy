65f9409d2894cf2a7578403b013fa7c56a53f1f8
Brett McGillis <brett.mcgillis@gmail.com>
2026-01-26 10:11:39 -0500

    put channels in dedicated hook

---
+++ src/components/Scenes/CRTTest/round2/useCableSubscription.js
@@ -0,0 +1,81 @@
+import React, { useMemo } from 'react';
+
+/* --- Materials --- */
+import CRTBlueScreenMaterial, {
+  TerminalSetting,
+  VHSSetting,
+} from 'components/scenes/CRTTest/CRTBlueScreenMaterial';
+import CRTSceneInSceneMaterial from 'components/scenes/CRTTest/CRTSceneInSceneMaterial';
+import CRTSceneMaterial from 'components/scenes/CRTTest/CRTSceneMaterial';
+import CRTShowMaterial from 'components/scenes/CRTTest/CRTShowMaterial';
+import CRTSnowMaterial from 'components/scenes/CRTTest/CRTSnowMaterial';
+import CRTStaticMaterial from 'components/scenes/CRTTest/CRTStaticMaterial';
+import TestScene from 'components/scenes/CRTTest/TestScene';
+
+function audioFile(name) {
+  return `${process.env.PUBLIC_URL}/audio/${name}`;
+}
+
+export default function useCableSubscription() {
+  const channels = useMemo(
+    () => [
+      { key: 'snow', video: <CRTSnowMaterial />, audio: null },
+      { key: 'static', video: <CRTStaticMaterial />, audio: null },
+      {
+        key: 'vhs',
+        video: (
+          <CRTBlueScreenMaterial
+            {...VHSSetting}
+            horizontalPadding={100}
+            verticalPadding={95}
+          />
+        ),
+        audio: null,
+      },
+      {
+        key: 'terminal',
+        video: (
+          <CRTBlueScreenMaterial
+            {...TerminalSetting}
+            horizontalPadding={100}
+            verticalPadding={95}
+          />
+        ),
+        audio: null,
+      },
+      {
+        key: 'homeVideo',
+        video: <CRTShowMaterial useWebcam />,
+        audio: {
+          type: 'file',
+          url: audioFile('laugh-track.mp3'),
+          loop: true,
+        },
+      },
+      {
+        key: 'tv',
+        video: <CRTShowMaterial />,
+        audio: {
+          type: 'file',
+          url: audioFile('ren-and-stimpy.mp3'),
+          loop: true,
+        },
+      },
+
+      /* 🔥 external audio channel */
+      {
+        key: 'threeD',
+        video: <CRTSceneMaterial scene={<TestScene />} />,
+        audio: {
+          type: 'external',
+          input: 'strudel',
+        },
+      },
+
+      { key: 'pip', video: <CRTSceneInSceneMaterial />, audio: null },
+    ],
+    []
+  );
+
+  return { channels };
+}

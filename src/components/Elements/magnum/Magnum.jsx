2412e2eba9739b4a2bc797fabbfd42d6096115a5
Brett McGillis <brett.mcgillis@gmail.com>
2026-03-21 22:30:39 -0400

    save for later

---
+++ src/components/Elements/magnum/Magnum.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import { useGLTF } from '@react-three/drei';
+
+import { modelFile } from '../../../utils/appUtils';
+
+export default function Magnum(props) {
+  const { nodes, materials } = useGLTF(modelFile('/44_magnum.glb'));
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.193}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_1.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_2.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_3.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_4.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_5.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_6.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_7.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_8.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_9.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_10.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_11.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_12.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_13.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.defaultMaterial_14.geometry}
+            material={materials['44_Magnum_MTL']}
+          />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload(modelFile('/44_magnum.glb'));

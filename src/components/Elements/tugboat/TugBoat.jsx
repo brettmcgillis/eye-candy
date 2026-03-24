3ace4c8a15af09daf23bf5b32eb5aa394f24679f
Brett McGillis <brett.mcgillis@gmail.com>
2026-03-24 19:24:11 -0400

    toot toot

---
+++ src/components/Elements/tugboat/TugBoat.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { useGLTF } from '@react-three/drei';
+
+import { modelFile } from '../../../utils/appUtils';
+
+export default function TugBoat(props) {
+  const { nodes, materials } = useGLTF(modelFile('/tugboat.glb'));
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        name="UpperDeck"
+        castShadow
+        receiveShadow
+        geometry={nodes.UpperDeck.geometry}
+        material={materials.Deck_Mat}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={0.01}
+      />
+      <mesh
+        name="LowerDeck"
+        castShadow
+        receiveShadow
+        geometry={nodes.LowerDeck.geometry}
+        material={materials.Body_Mat}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={0.01}
+      />
+      <mesh
+        name="LowerDeck_Glass"
+        castShadow
+        receiveShadow
+        geometry={nodes.LowerDeck_Glass.geometry}
+        material={materials.Body_Mat}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={0.01}
+      />
+      <mesh
+        name="UpperDeck_Glass"
+        castShadow
+        receiveShadow
+        geometry={nodes.UpperDeck_Glass.geometry}
+        material={materials.Deck_Mat}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={0.01}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload(modelFile('/tugboat.glb'));

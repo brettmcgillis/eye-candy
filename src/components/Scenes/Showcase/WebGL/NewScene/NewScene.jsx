3452cc0a9c90e6a9e506e24dfadfa46c23def916
Brett McGillis <brett.mcgillis@gmail.com>
2026-03-26 20:39:00 -0400

    doc'd elswhere

--- src/components/Scenes/Showcase/WebGL/NewScene/NewScene.jsx
+++ src/components/Scenes/Showcase/WebGL/NewScene/NewScene.jsx
@@ -127,9 +127,6 @@ function useSceneControls() {
             label: 'Ascii Char Set',
             value:
               " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@",
-            // " ░▒▓█"
-            // "  ▁▂▃▄▅▆▇█"
-            // "  ░▒▓█▁▂▃▄▅▆▇█"
           },
           asciiInvert: { label: 'Ascii Invert', value: false },
           asciiColorize: { label: 'Ascii Colorize', value: true },

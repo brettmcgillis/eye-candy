e8ac3bbc345de98bd26e9e6bb4a4767ffb8afb40
Brett McGillis <brett.mcgillis@gmail.com>
2026-03-31 19:25:10 -0400

    simplify

--- src/components/Elements/45lbPlate/45lbPlate.jsx
+++ src/components/Elements/45lbPlate/45lbPlate.jsx
@@ -8,16 +8,14 @@ export default function Lb45Plate(props) {
   const { nodes, materials } = useGLTF(modelFile('/45lb_metal_plate.glb'));
   return (
     <group {...props} dispose={null}>
-      <group scale={0.01}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes['45lb_Plate_Material001_0'].geometry}
-          material={materials['Material.001']}
-          rotation={[0, 0, -0.784]}
-          scale={100}
-        />
-      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes['45lb_Plate_Material001_0'].geometry}
+        material={materials['Material.001']}
+        rotation={[0, 0, -0.784]}
+        scale={10}
+      />
     </group>
   );
 }

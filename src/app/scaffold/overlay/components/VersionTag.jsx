daf44afdb37db1b72a7ea20c7500585a8fa66287
Brett McGillis <brett.mcgillis@gmail.com>
2026-05-05 13:02:54 -0400

    tag

--- src/app/scaffold/overlay/components/VersionTag.jsx
+++ src/app/scaffold/overlay/components/VersionTag.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default function VersionTag() {
   const version = __APP_VERSION__;
-  return <div className="version-tag">v. {version}</div>;
+  return <div className="version-tag">{`v. ${version}`}</div>;
 }

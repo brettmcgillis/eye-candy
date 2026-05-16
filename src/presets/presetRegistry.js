afc613cd8eae768b2bde1182b47a7c34c1edab3f
Brett McGillis <brett.mcgillis@gmail.com>
2026-05-15 20:31:34 -0400

    tidy

--- src/presets/presetRegistry.js
+++ src/presets/presetRegistry.js
@@ -18,11 +18,6 @@ export const PRESET_REGISTRY = {
     'Flying High': 'src/presets/fire/flyingHighFire.js',
     'Police Presence': 'src/presets/fire/policePresenceFire.js',
   },
-  cloth: {
-    Default: 'src/presets/cloth/defaultCloth.js',
-    Ghost: 'src/presets/cloth/ghostCloth.js',
-    Ribbon: 'src/presets/cloth/ribbonCloth.js',
-  },
 };
 
 /**

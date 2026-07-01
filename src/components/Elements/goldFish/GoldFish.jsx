fad0d966d520b034c19a61cd8b91edec2cf0ab61
Brett McGillis <brett.mcgillis@gmail.com>
2026-07-01 15:47:28 -0400

    some todos and a nice fishy

--- src/components/Elements/goldFish/GoldFish.jsx
+++ src/components/Elements/goldFish/GoldFish.jsx
@@ -1,28 +1,108 @@
+/* eslint-disable no-underscore-dangle */
+import { SkeletonUtils } from 'three-stdlib';
+
 import React from 'react';
 
-import { useGLTF } from '@react-three/drei';
+import { useAnimations, useGLTF } from '@react-three/drei';
+import { useGraph } from '@react-three/fiber';
 
 import { modelFile } from '../../../utils/appUtils';
 
-export function GoldFish(props) {
-  const { nodes, materials } = useGLTF(modelFile('/goldfish.glb'));
+export default function GoldFish(props) {
+  const group = React.useRef();
+  const { scene, animations } = useGLTF(modelFile('goldfish.glb'));
+  const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene]);
+  const { nodes, materials } = useGraph(clone);
+  const { actions } = useAnimations(animations, group);
   return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.033}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.fish_Material006_0.geometry}
-            material={materials['Material.006']}
-            position={[0, 29.917, 18.927]}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={9.747}
-          />
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Object_12" rotation={[-Math.PI / 2, 0, 0]} scale={100}>
+          <primitive object={nodes._rootJoint} />
         </group>
+        <skinnedMesh
+          name="Object_15"
+          geometry={nodes.Object_15.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_15.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_17"
+          geometry={nodes.Object_17.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_17.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_19"
+          geometry={nodes.Object_19.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_19.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_21"
+          geometry={nodes.Object_21.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_21.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_23"
+          geometry={nodes.Object_23.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_23.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_25"
+          geometry={nodes.Object_25.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_25.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_27"
+          geometry={nodes.Object_27.geometry}
+          material={materials.M_CometEyes}
+          skeleton={nodes.Object_27.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_75"
+          geometry={nodes.Object_75.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_75.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_77"
+          geometry={nodes.Object_77.geometry}
+          material={materials.M_CometSp}
+          skeleton={nodes.Object_77.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
+        <skinnedMesh
+          name="Object_79"
+          geometry={nodes.Object_79.geometry}
+          material={materials.M_CometEyes}
+          skeleton={nodes.Object_79.skeleton}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={100}
+        />
       </group>
     </group>
   );
 }
 
-useGLTF.preload(modelFile('/goldfish.glb'));
+useGLTF.preload(modelFile('goldfish.glb'));
--- src/components/Scenes/WorkInProgress/WebGPU/MyHeartIsABrokenFishTank/todo.md
+++ src/components/Scenes/WorkInProgress/WebGPU/MyHeartIsABrokenFishTank/todo.md
@@ -13,6 +13,16 @@
 - [x] Add spill/drain boundary opening prototype
 - [x] Add fish state transitions for draining and stranding
 - [x] Replace spill/drain prototype with a dynamic fluid domain simulation
+- [ ] water spill on table shoud be more circular and less square to be more puddle like.
+- [ ] Add overlay buttons to reset scene
+- [ ] user interaction with water. see dev/examples/three.js -> examples -> webgpu_compute_particles_fluid
+- [ ] add click+drag to move rocks & broken glass, like in dumpsterfire. still support ricochet, like dumpsterfire
+- [ ] fix fish animation looks bad.
+- [ ] update scene to create unique breaks each time scene loads or scene is reset. Make this easy to disable in code incase I want to make a video thats consistent.
+- [ ] use the updated, animated GoldFish component and model.
+- [ ] fix debug water bounds. cant see water when enabled.
+- [ ] Can we prevent particles from spilling before glass breaks?
+- [ ] wtf is going on wth the coral in the tank. looks black from most angles, until suddenly it becomes colored at some angle.
 
 # // Intent/Use Cases
 
@@ -44,6 +54,6 @@
 
 # // Bugs
 
-- [ ] Tune fish and tank scales against the authored assets once the scene is in active development
+- [ ] Tune fish and tank scales. tank bigger. fish much bigger
 - [ ] Tank and table physics now use approximate hull/cuboid colliders rather than hand-authored collider volumes
 - [ ] Spill remains a table-aware height-field surface and does not yet react to rigid-body rocks or glass fragments

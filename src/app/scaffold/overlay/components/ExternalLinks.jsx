7a9280a39eac4b78a95d5925f412669bceaff961
Brett McGillis <brett.mcgillis@gmail.com>
2026-07-01 09:48:04 -0400

    tap to show hide links

--- src/app/scaffold/overlay/components/ExternalLinks.jsx
+++ src/app/scaffold/overlay/components/ExternalLinks.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+import React, { useState } from 'react';
 import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
-import { localEnv } from '../../../../utils/appUtils';
-
 export default function ExternalLinks() {
-  const local = localEnv();
+  const [showIcons, setShowIcons] = useState(false);
 
   return (
     <div className="external-links">
-      <span>Brett McGillis</span>
-      {!local && (
-        <>
-          <a
-            href="https://www.instagram.com/ruinedpaintings/"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Instagram"
-          >
-            <FaInstagram color="#000000" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/brett-mcgillis-61b93a125/"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedin color="#000000" />
-          </a>
-        </>
-      )}
+      <span
+        className="external-links-name"
+        onClick={() => setShowIcons((s) => !s)}
+        role="button"
+        aria-expanded={showIcons}
+      >
+        Brett McGillis
+      </span>
+      <span className={`external-links-icons${showIcons ? ' is-visible' : ''}`}>
+        <a
+          href="https://www.instagram.com/ruinedpaintings/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram color="#000000" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/brett-mcgillis-61b93a125/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin color="#000000" />
+        </a>
+      </span>
     </div>
   );
 }

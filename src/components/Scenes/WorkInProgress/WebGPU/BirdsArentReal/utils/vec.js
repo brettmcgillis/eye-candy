216a8bf83d1fe01cd9901320907d60c1816ba462
Brett McGillis <brett.mcgillis@gmail.com>
2026-07-01 11:08:39 -0400

    move birds arent real

--- src/components/Scenes/WorkInProgress/WebGPU/BirdsArentReal/utils/vec.js
+++
@@ -1,5 +0,0 @@
-// Leva vec3 controls hand back `{ x, y, z }` objects; three props want arrays.
-// One tiny converter shared by the scene + its prop-driven child components.
-const v3 = (o) => [o.x, o.y, o.z];
-
-export default v3;
